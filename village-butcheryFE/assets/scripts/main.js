319f282d85d98bbed839c1e67d69fbc5469f3bef
Ani Stanley <ani.stanley.kelechi@bubble.ng>
2020-10-29 12:31:41 +0100

    add customer page

---
+++ village-butcheryFE/assets/scripts/main.js
@@ -0,0 +1,33 @@
+$('.autoplay').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
